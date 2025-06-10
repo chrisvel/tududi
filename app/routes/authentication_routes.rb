0c8d0da6ba0f28c5027385b7295c127b3bf9a306
Your Name <you@example.com>
2025-06-10 23:40:28 +0300

    Remove old routes

--- app/routes/authentication_routes.rb
+++ app/routes/authentication_routes.rb
@@ -43,34 +43,6 @@ class Sinatra::Application
     status 200
     { message: 'Logged out successfully' }.to_json
   end
-
-  # post '/login' do
-  #   content_type :json
-  #   request_payload = begin
-  #     JSON.parse(request.body.read)
-  #   rescue StandardError
-  #     nil
-  #   end
-
-  #   if request_payload
-  #     email = request_payload['email']
-  #     password = request_payload['password']
-  #   else
-  #     halt 400, { error: 'Invalid login parameters.' }.to_json
-  #   end
-
-  #   user = User.find_by(email: email)
-  #   if user&.authenticate(password)
-  #     session[:user_id] = user.id
-  #     status 200
-  #     { user: { email: user.email, id: user.id, language: user.language, appearance: user.appearance,
-  #               timezone: user.timezone } }.to_json
-  #   else
-  #     halt 401, { errors: ['Invalid credentials'] }.to_json
-  #   end
-  # end
-
-  # get '/logout' do
   #   session.clear
   #   redirect '/login'
   # end
