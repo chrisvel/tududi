cec73a6f250e0d0185ab80f2127e564f1357a5c8
Chris Veleris <chrisveleris@gmail.com>
2025-10-04 22:13:23 +0300

    fixup! Fix breaking E2E tests

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -9,7 +9,7 @@ const { Op } = require('sequelize');
 const { extractUidFromSlug } = require('../utils/slug-utils');
 const { validateTagName } = require('../services/tagsService');
 const { uid } = require('../utils/uid');
-const { logError } = require("../services/logService");
+const { logError } = require('../services/logService');
 const router = express.Router();
 
 // Helper function to safely format dates
