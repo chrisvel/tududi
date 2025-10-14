d60ed23f183b4d84feb6cc696a415f6739638cbe
Chris Veleris <chrisveleris@gmail.com>
2025-10-14 12:58:10 +0300

    Improve migrations safe functions usage

--- backend/migrations/20251011000001-add-surname-to-users.js
+++ backend/migrations/20251011000001-add-surname-to-users.js
@@ -1,14 +1,25 @@
 'use strict';
 
+const {
+    safeAddColumns,
+    safeRemoveColumn,
+} = require('../utils/migration-utils');
+
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    up: async (queryInterface, Sequelize) => {
-        await queryInterface.addColumn('users', 'surname', {
-            type: Sequelize.STRING,
-            allowNull: true,
-        });
+    async up(queryInterface, Sequelize) {
+        await safeAddColumns(queryInterface, 'users', [
+            {
+                name: 'surname',
+                definition: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                },
+            },
+        ]);
     },
 
-    down: async (queryInterface) => {
-        await queryInterface.removeColumn('users', 'surname');
+    async down(queryInterface) {
+        await safeRemoveColumn(queryInterface, 'users', 'surname');
     },
 };
