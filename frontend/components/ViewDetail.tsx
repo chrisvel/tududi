2424887df9648da46e6f257f63b36b636b3ef492
Chris Veleris <chrisveleris@gmail.com>
2025-11-17 18:32:03 +0200

    fixup! fixup! Add pagination to smart views

--- frontend/components/ViewDetail.tsx
+++ frontend/components/ViewDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import {
