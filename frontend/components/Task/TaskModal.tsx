575e5d8c1994e5ca98e51b72eb50a24806c79736
Chris Veleris <chrisveleris@gmail.com>
2025-11-17 13:40:34 +0200

    fixup! Debounce task save button

--- frontend/components/Task/TaskModal.tsx
+++ frontend/components/Task/TaskModal.tsx
@@ -1032,7 +1032,10 @@ const TaskModal: React.FC<TaskModalProps> = ({
                                                         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                                                     ></path>
                                                 </svg>
-                                                {t('common.saving', 'Saving...')}
+                                                {t(
+                                                    'common.saving',
+                                                    'Saving...'
+                                                )}
                                             </span>
                                         ) : (
                                             t('common.save', 'Save')
