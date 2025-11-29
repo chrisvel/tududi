e3650f44de3fd62844be49b09fb1c4a59b02b5a4
Chris Veleris <chrisveleris@gmail.com>
2025-11-29 09:03:11 +0200

    fixup! Add file attacmhments

--- backend/migrations/20251128000001-create-task-attachments.js
+++ backend/migrations/20251128000001-create-task-attachments.js
@@ -82,7 +82,7 @@ module.exports = {
         });
     },
 
-    async down(queryInterface, Sequelize) {
+    async down(queryInterface) {
         await queryInterface.dropTable('task_attachments');
     },
 };
