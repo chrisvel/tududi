95c76daaf7c6bf1306bf8a37fd5dd5e8b279c0fb
Chris <chrisvel@users.noreply.github.com>
2025-12-03 17:05:25 +0200

    Fix flag issue (#641)

--- backend/routes/tasks/utils/logging.js
+++ backend/routes/tasks/utils/logging.js
@@ -12,6 +12,7 @@ function captureOldValues(task) {
         due_date: task.due_date,
         project_id: task.project_id,
         note: task.note,
+        today: task.today,
         recurrence_type: task.recurrence_type,
         recurrence_interval: task.recurrence_interval,
         recurrence_end_date: task.recurrence_end_date,
