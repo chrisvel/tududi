86398f0a555fef20ef505df74d8a8190ef771669
Chris Veleris <chrisveleris@gmail.com>
2026-09-13 09:05:40 +0300

    Update tududi logo link

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -214,15 +214,21 @@ const Login: React.FC = () => {
             {/* Navbar */}
             <nav className="fixed top-0 left-0 right-0 z-50 text-gray-900 dark:text-white">
                 <div className="h-16 flex items-center px-4 sm:px-6 lg:px-8">
-                    <img
-                        src={getAssetPath(
-                            isDarkMode
-                                ? 'wide-logo-light.png'
-                                : 'wide-logo-dark.png'
-                        )}
-                        alt="tududi"
-                        className="h-9 w-auto"
-                    />
+                    <a
+                        href="https://tududi.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            src={getAssetPath(
+                                isDarkMode
+                                    ? 'wide-logo-light.png'
+                                    : 'wide-logo-dark.png'
+                            )}
+                            alt="tududi"
+                            className="h-9 w-auto"
+                        />
+                    </a>
                 </div>
             </nav>
 
